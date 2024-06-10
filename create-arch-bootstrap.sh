df605bffad96fbc26af8a1343ffbd76189f6e526
Laith <81252120+LaithOsama@users.noreply.github.com>
2024-06-10 08:57:41 +0000

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -9,12 +9,12 @@
 audio_pkgs="alsa-lib lib32-alsa-lib alsa-plugins lib32-alsa-plugins libpulse \
 	lib32-libpulse jack2 lib32-jack2 alsa-tools alsa-utils pipewire lib32-pipewire"
 
-video_pkgs="mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
-	vulkan-intel lib32-vulkan-intel nvidia-utils lib32-nvidia-utils \
+video_pkgs="mesa lib32-mesa  \
+	vulkan-intel lib32-vulkan-intel \
 	vulkan-icd-loader lib32-vulkan-icd-loader vulkan-mesa-layers \
 	lib32-vulkan-mesa-layers libva-mesa-driver lib32-libva-mesa-driver \
 	libva-intel-driver lib32-libva-intel-driver intel-media-driver \
-	mesa-utils vulkan-tools nvidia-prime libva-utils lib32-mesa-utils"
+	mesa-utils vulkan-tools libva-utils lib32-mesa-utils"
 
 wine_pkgs="wine-tkg-staging-fsync-git winetricks-git wine-nine wineasio \
 	giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap \
@@ -28,24 +28,24 @@ wine_pkgs="wine-tkg-staging-fsync-git winetricks-git wine-nine wineasio \
 	gst-plugins-ugly gst-plugins-base lib32-gst-plugins-good \
 	lib32-gst-plugins-base gst-libav wget faudio lib32-faudio"
 
-devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
+devel_pkgs=""
 
 # Packages to install
 # You can add packages that you want and remove packages that you don't need
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
 export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
-	nano ttf-dejavu ttf-liberation lutris steam firefox mpv geany pcmanfm \
-	htop qbittorrent speedcrunch gpicview file-roller xorg-xwayland \
-	steam-native-runtime gamemode lib32-gamemode jre17-openjdk lxterminal \
+	vi ttf-dejavu ttf-liberation lutris steam  mpv pcmanfm \
+	htop xorg-xwayland chromium \
+	steam-native-runtime gamemode lib32-gamemode jre17-openjdk \
 	steamtinkerlaunch mangohud lib32-mangohud qt6-wayland wayland \
-	lib32-wayland qt5-wayland retroarch xorg-server-xephyr openbox \
-	obs-studio gamehub minigalaxy legendary gamescope prismlauncher yt-dlp \
+	lib32-wayland qt5-wayland retroarch xorg-server-xephyr \
+	obs-studio gamehub minigalaxy legendary gamescope prismlauncher \
 	bottles playonlinux minizip retroarch-assets-ozone libretro-beetle-psx-hw \
 	libretro-blastem libretro-bsnes libretro-dolphin libretro-duckstation \
 	libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia \
 	libretro-parallel-n64 libretro-pcsx2 libretro-picodrive libretro-ppsspp \
-	libretro-retrodream libretro-yabause sunshine"
+	libretro-retrodream libretro-yabause dwarfs fuse-overlayfs"
 
 # If you want to install AUR packages, specify them in this variable
 export aur_packagelist=""
